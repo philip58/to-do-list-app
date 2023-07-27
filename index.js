8207a764e7defe48c710fbba3f93fba267381699
Philip Fernandez <philipfernandezz@gmail.com>
2023-07-27 17:38:30 -0400

    backend post requesting

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var listItemsToday = [];
+var listItemsTomorrow = [];
 
 app.get("/",(req,res) => {
-    res.render("index.ejs");
+    res.render("index.ejs",{newListItem: listItemsToday});
 });
 
 app.get("/tomorrow",(req,res) => {
-    res.render("tomorrow.ejs");
+    res.render("tomorrow.ejs",{newListItem: listItemsTomorrow});
+});
+
+app.post("/",(req,res) => {
+    var listItemToday = req.body.newItem;
+    listItemsToday.push(listItemToday);
+    res.redirect("/");
+});
+
+app.post("/tomorrow",(req,res) => {
+    var listItemTomorrow = req.body.newItem;
+    listItemsTomorrow.push(listItemTomorrow);
+    res.redirect("/tomorrow");
 });
 
 app.listen(port, () => {
